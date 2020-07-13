d7de4798c95d6e92fe1ed8e9cbf4243de946a332
Salutin <wujinyi@sjtu.edu.cn>
2020-07-14 00:54:52 +0800

    mvp

--- src/Update.elm
+++ src/Update.elm
@@ -4,6 +4,7 @@ import Model exposing (..)
 import Random
 import Svg exposing (animate)
 import Html exposing (time)
+import Svg.Attributes exposing (direction)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -74,6 +75,7 @@ stand player =
     { player| anim = Stand, frame = 0, speed = Vector 0 0 }
 
 jump player =
+    
     case player.direction of
         Left ->
             { player| anim = Jump, frame = 0, speed = Vector -0.1 -0.2 }
@@ -81,11 +83,13 @@ jump player =
             { player| anim = Jump, frame = 0, speed = Vector 0.1 -0.2 }
 
 walk moveDirection player = 
-    case moveDirection of
-        Left ->
-            { player| anim = Walk,  direction= moveDirection, speed = Vector -0.1 0 }
-        Right ->
-            { player| anim = Walk,  direction= moveDirection, speed = Vector 0.1 0 }
+    if player.anim == Stand then
+        case moveDirection of
+            Left ->
+                { player| anim = Walk,  direction= moveDirection, speed = Vector -0.1 0 }
+            Right ->
+                { player| anim = Walk,  direction= moveDirection, speed = Vector 0.1 0 }
+    else player
 
 animate time model =
     let
@@ -105,34 +109,59 @@ onFloor bricks time player=
         (((player.pos.y2 + player.speed.y*time)>(4000)))
         ||(onBricks player time posList)
 
+underFloor bricks time player =
+    let
+        posList = List.map .pos bricks
+    in
+        underBricks player time posList
 onBricks player time posList =
-    (posList|> List.filter (\pos -> (pos.x1<player.pos.x1) && (player.pos.x2-100<pos.x2))
-            |> List.filter (\pos -> (player.pos.y2-25<pos.y1) && ((player.pos.y2-25 + 2)>pos.y1))
+    (posList|> List.filter (\pos -> (pos.x1<=player.pos.x1+80) && (player.pos.x2-100<=pos.x2))
+            |> List.filter (\pos -> (player.pos.y2-45<pos.y1) && ((player.pos.y2-45 + 10)>pos.y1))
+            |> List.isEmpty) == False
+
+underBricks player time posList =
+    (posList|> List.filter (\pos -> (pos.x1<=player.pos.x1+80) && (player.pos.x2-100<=pos.x2))
+            |> List.filter (\pos -> (player.pos.y1+40<pos.y2) && ((player.pos.y1 + 50)>pos.y2))
             |> List.isEmpty) == False
 
 changeAnim map time player=
-    if player.anim == Jump && (player |> onFloor map time) then
+    if (player.anim == Jump && (player |> onFloor map time) && player.speed.y>0) then--||onWall map time player then
         player |> stand
     else player
 
-
-
-changeSpeed time map player =
+onWall bricks time player= 
     let
         minX = toFloat(floor (player.pos.x1/1600) * 1600)
         maxX = minX + 1600
-        dx = if ((player.speed.x * time + player.pos.x1) >= minX 
-                && (player.speed.x * time + player.pos.x2-100) <= maxX)then
-                0
-            else
+        posList = List.map .pos bricks
+    in
+        ((player.speed.x * time + player.pos.x1) < minX 
+                || (player.speed.x * time + player.pos.x2-100) > maxX)
+        ||(touchBricks player time posList)
+
+touchBricks player time posList =
+    (posList|> List.filter (\pos -> (pos.y1<=player.pos.y1+100) && (player.pos.y2-100<=pos.y2))
+            |> List.filter (\pos -> ((player.pos.x1+40>pos.x2) 
+                                    && (player.speed.x * time + player.pos.x1+40<pos.x2))
+                                    ||((player.pos.x2-120<pos.x1) 
+                                    && (player.speed.x * time + player.pos.x2-120>pos.x1)))
+            |> List.isEmpty) == False
+
+changeSpeed time map player =
+    let
+    
+        dx = if onWall map time player then
                 -2 * player.speed.x
-        dy = if (player |> onFloor map time) then
-                if  player.speed.y > 0 then
+            else
+                0
+        dy = if (player |> underFloor map time) && player.speed.y < 0 then
+                    -2* player.speed.y
+            else if (player |> onFloor map time) && player.speed.y > 0  then
                     -player.speed.y
-                else 
+            else if (player |> onFloor map time) && player.speed.y <= 0 then
                     0
             else 
-                0.0002 * time
+                    0.0002 * time
         speed = Vector (player.speed.x + dx) ( player.speed.y + dy) 
     in
         {player | speed = speed}
